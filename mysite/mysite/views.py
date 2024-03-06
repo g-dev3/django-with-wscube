4614eeb1ccb371fe2473f23d67a36bb1e522c0b1
G Dev3 <gdev3.js@gmail.com>
2024-03-06 21:54:01 +0530

    fetch data with for loop in html

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -5,7 +5,14 @@ def homePage(request):
     data={
         'title': 'Home Page',
         'bdata':'G Developer site...',
-        'clist': ['PHP','Java','Javascript']
+        'clist': ['Html','Css','Javascript','C','Python','Django','MySQL','Git','Github','Ms-excel'],
+        'student_details':[
+            {'name':'Robin','age':21,'phone':9876543210},
+            {'name':'Toni','age':37,'phone':1234567890},
+            {'name':'Jasmin','age':18,'phone':9988667733},
+            {'name':'Kaniya','age':24,'phone':8877755999},
+            {'name':'Robin 2','age':20,'phone':9988544444},
+        ]
     }
     return render(request,"index.html",data)
 
